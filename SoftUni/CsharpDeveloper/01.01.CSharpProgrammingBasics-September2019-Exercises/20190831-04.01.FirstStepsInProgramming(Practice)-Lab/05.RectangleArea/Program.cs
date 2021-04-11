c82f966fa2f6806ec627707568c61b061c1f87f7
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-11 15:14:45 +0300

    05.RectangleArea

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190831-04.01.FirstStepsInProgramming(Practice)-Lab/05.RectangleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05.RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            double area = a * b;
+            Console.WriteLine("Area = " + area);
+        }
+    }
+}
