a0c219e15aca22d2de4bc283e9b67aa858dd2542
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-13 23:22:41 +0300

    11. USD to BGN

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190907-05.01.SimpleCalculations(Practice)-Lab/11.USDtoBGN/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _11.USDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double USD = double.Parse(Console.ReadLine());
+            double BGN = USD * 1.79549;
+            Console.WriteLine(Math.Round(USD * 1.79549, 2));
+
+            //Console.Write("USD = ");
+            //double usd = double.Parse(Console.ReadLine());
+            //double bgn = usd * 1.79549;
+            //Console.WriteLine("BGN = " + Math.Round(bgn, 2));
+        }
+    }
+}
