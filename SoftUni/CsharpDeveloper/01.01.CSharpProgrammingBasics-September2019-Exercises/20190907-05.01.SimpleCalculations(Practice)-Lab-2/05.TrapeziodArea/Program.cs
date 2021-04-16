6e0e7fce0d0a6deda8af121cb333597c61c65772
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-16 06:20:04 +0300

    05. Trapeziod Area

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190907-05.01.SimpleCalculations(Practice)-Lab-2/05.TrapeziodArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideTrapezoidA = double.Parse(Console.ReadLine());
+            double sideTrapezoidB = double.Parse(Console.ReadLine());
+            double heightTrapezoid = double.Parse(Console.ReadLine());
+
+            double trapezoidArea = ((sideTrapezoidA + sideTrapezoidB) / 2) * heightTrapezoid;
+            Console.WriteLine(trapezoidArea);
+        }
+    }
+}
