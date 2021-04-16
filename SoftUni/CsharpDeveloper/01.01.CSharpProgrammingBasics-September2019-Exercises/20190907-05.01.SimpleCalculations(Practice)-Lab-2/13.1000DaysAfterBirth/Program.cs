28ee5be73e84341c8c7e6735d2e2905f786fa76d
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-16 06:23:28 +0300

    13. 1000 Days After Birth

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190907-05.01.SimpleCalculations(Practice)-Lab-2/13.1000DaysAfterBirth/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _13._1000DaysAfterBirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateString = Console.ReadLine();
+            string format = "dd-MM-yyyy";
+            DateTime result = DateTime.ParseExact(dateString, format, System.Globalization.CultureInfo.InvariantCulture);
+            System.Globalization.CultureInfo provider = System.Globalization.CultureInfo.InvariantCulture;
+            DateTime output = Convert.ToDateTime(result).AddDays(999);
+            Console.WriteLine(output.ToString("dd-MM-yyyy"));
+
+
+
+            //string BirthDate = Console.ReadLine();
+            //string format = "dd-MM-yyyy";
+            //DateTime time = DateTime.ParseExact(BirthDate, format, System.Globalization.CultureInfo.InvariantCulture);
+            //Console.WriteLine(time.AddDays(999).ToString(format));
+
+
+
+            //DateTime today = DateTime.Now;
+            //DateTime date = System.DateTime.Parse(Console.ReadLine());
+            //DateTime result = date.AddDays(1000);
+            //Console.WriteLine("{0:dd-MM-yyyy}", result);
+        }
+    }
+}
