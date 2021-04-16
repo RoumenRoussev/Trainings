822cd9e0689319012408ee6a8a6907aef45f547b
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-16 06:19:18 +0300

    02. Inches to Centimeters

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190907-05.01.SimpleCalculations(Practice)-Lab-2/02.InchesToCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04._02.InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double centimetre = double.Parse(Console.ReadLine());
+            double inch = 2.54;
+            Console.WriteLine($"{centimetre * inch:F2}");
+        }
+    }
+}
