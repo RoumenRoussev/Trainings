eb162818fc8472a0039154a5e28d286e936af527
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-18 11:51:17 +0300

    02. Vegetable Market

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190907-05.02.SimpleCalculations(Practice)-ExamProblems/02.VegetableMarket/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceVeges = double.Parse(Console.ReadLine());
+            double priceFruits = double.Parse(Console.ReadLine());
+
+            double kgVeges = double.Parse(Console.ReadLine());
+            double kgFruits = double.Parse(Console.ReadLine());
+
+            double valEUR = 1.94;
+            double total = ((priceVeges * kgVeges) + (priceFruits * kgFruits)) / valEUR;
+
+            Console.WriteLine($"{total:F2}");
+        }
+    }
+}
