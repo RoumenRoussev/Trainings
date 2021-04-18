a39ffe6af32fbc9d196a6bd4625e3899957a4dd2
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-18 11:51:04 +0300

    01. Training Lab

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190907-05.02.SimpleCalculations(Practice)-ExamProblems/01.TrainingLab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Math.Floor((double.Parse(Console.ReadLine()) * 100) / 120);
+            var b = Math.Floor((double.Parse(Console.ReadLine()) * 100 - 100) / 70);
+            Console.WriteLine(a * b - 3);
+        }
+    }
+}
