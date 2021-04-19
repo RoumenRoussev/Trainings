07bbf3ef6756918d5a6cde93c0e05bd8d9d12f17
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-19 17:04:34 +0300

    09. Fish Tank

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.01.SimpleOperationsandCalculations(Exercises)-Lab/09.FishTank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09.FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+            double persentageCount = percentage * 0.01;
+            double litre = 1.0 / 1000.0;
+            int volume = length * width * height;
+            double litresSum = volume * litre;
+            double litresVolume = litresSum * (1 - persentageCount);
+            Console.WriteLine($"{litresVolume:f3}");
+        }
+    }
+}
