85cc5b8735c5f69d198e23b620b5073cc97cf311
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-19 17:03:35 +0300

    07. Pet Shop

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.01.SimpleOperationsandCalculations(Exercises)-Lab/07.PetShop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07.PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double foodDogs = 2.50;
+            double foodRestDogs = 4.00;
+            int numDogs = int.Parse(Console.ReadLine());
+            int numRestDogs = int.Parse(Console.ReadLine());
+            double totalDogs = foodDogs * numDogs;
+            double totalRestDogs = foodRestDogs * numRestDogs;
+            double total = totalDogs + totalRestDogs;
+            Console.WriteLine($"{total:F2} lv.");
+        }
+    }
+}
