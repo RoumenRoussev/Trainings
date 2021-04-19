adfa502d32048ad911383dea43bd9f509f2c755e
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-19 17:03:25 +0300

    06. Circle Area and Perimeter

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.01.SimpleOperationsandCalculations(Exercises)-Lab/06.CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06.CircleAreaAndPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double circleRadius = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * Math.Pow(circleRadius, 2);
+            double perimeter = 2 * Math.PI * circleRadius;
+
+            Console.WriteLine($"{area:f2}");
+            Console.WriteLine($"{perimeter:f2}");
+        }
+    }
+}
