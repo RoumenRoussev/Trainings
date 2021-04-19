eccd4a44fc0f2d3965a16bc3c6d898a42241d843
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-19 17:03:13 +0300

    05. Projects Creation

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.01.SimpleOperationsandCalculations(Exercises)-Lab/05.ProjectsCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05.ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string archName = Console.ReadLine();
+            int numHours = 3;
+            int numProject = int.Parse(Console.ReadLine());
+            int sumHours = numHours * numProject;
+            Console.WriteLine($"The architect {archName} will need {sumHours} hours to complete {numProject} project/s.");
+        }
+    }
+}
