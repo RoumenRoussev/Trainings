5a966f6f03f881834440bbce4efb89a439ec6f6f
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-19 17:04:23 +0300

    08. Yard Greening

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.01.SimpleOperationsandCalculations(Exercises)-Lab/08.YardsLandscaping/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08.YardsLandscaping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareMetre = double.Parse(Console.ReadLine());
+            double priceSquareMetre = 7.61;
+            double sumPrice = squareMetre * priceSquareMetre;
+            double discount = sumPrice * 0.18;
+            double finalPrice = sumPrice - discount;
+
+            Console.WriteLine($"The final price is: {finalPrice:F2} lv.");
+            Console.WriteLine($"The discount is: {discount:F2} lv.");
+        }
+    }
+}
