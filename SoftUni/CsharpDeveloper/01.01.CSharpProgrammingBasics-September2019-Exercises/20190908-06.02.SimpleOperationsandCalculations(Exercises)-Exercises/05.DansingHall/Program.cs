a287863e7379e2b8873543ee9f9e9169bcd49ddc
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-21 12:00:34 +0300

    05. Dance Hall

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.02.SimpleOperationsandCalculations(Exercises)-Exercises/05.DansingHall/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05.DansingHall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lengthHall = double.Parse(Console.ReadLine());
+            double widthHall = double.Parse(Console.ReadLine());
+            double wardrobeSide = double.Parse(Console.ReadLine());
+            var hallArea = (lengthHall * 100) * (widthHall * 100);
+            var wardrobe = (wardrobeSide * 100) * (wardrobeSide * 100);
+            double bench = (hallArea / 10);
+            double dancingfloor = hallArea - wardrobe - bench;
+            double dancers = (dancingfloor / (40 + 7000));
+            Console.WriteLine($"{Math.Floor(dancers)}");
+        }
+    }
+}
