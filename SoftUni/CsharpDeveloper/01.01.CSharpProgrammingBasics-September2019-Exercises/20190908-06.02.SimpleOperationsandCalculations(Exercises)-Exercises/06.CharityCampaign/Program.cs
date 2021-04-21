69b6b8a115aef4ffa680aa7b03b35bd43344ceb7
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-21 12:00:44 +0300

    06. Charity Campaign

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.02.SimpleOperationsandCalculations(Exercises)-Exercises/06.CharityCampaign/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int confectioner = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+            int moneyc = cakes * 45;
+            double moneywaffles = waffles * 5.80;
+            double moneypancakes = pancakes * 3.20;
+            double moneyday = (moneyc + moneywaffles + moneypancakes) * confectioner;
+            double moneyall = moneyday * days;
+            double final = moneyall - (moneyall / 8);
+            Console.WriteLine("{0:F2}", final);
+        }
+    }
+}
