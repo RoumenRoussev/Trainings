dd65b2259fac7db25ca8ce3ab3ed333025f87334
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-21 12:00:55 +0300

    07. Alcohol Market

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.02.SimpleOperationsandCalculations(Exercises)-Exercises/07.AlcoholMarket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.AlcoholMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double whiskeyPriceL = double.Parse(Console.ReadLine());
+            double beerL = double.Parse(Console.ReadLine());
+            double vineL = double.Parse(Console.ReadLine());
+            double brandyL = double.Parse(Console.ReadLine());
+            double whiskeyL = double.Parse(Console.ReadLine());
+
+            double brandyPriceL = whiskeyPriceL / 2;
+            double vinePriceL = brandyPriceL - (0.4 * brandyPriceL);
+            double beerPriceL = brandyPriceL - (0.8 * brandyPriceL);
+
+            double brandyPrice = brandyL * brandyPriceL;
+            double vinePrice = vineL * vinePriceL;
+            double beerPrice = beerL * beerPriceL;
+            double whiskeyPrice = whiskeyPriceL * whiskeyL;
+
+            double summaryPrice = brandyPrice + vinePrice + beerPrice + whiskeyPrice;
+
+            Console.WriteLine($"{summaryPrice:F2}");
+        }
+    }
+}
