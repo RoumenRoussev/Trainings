a159cceabcd2efedaaaf744b67f89fae7545803c
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-21 12:00:15 +0300

    03. 2D Rectangle Area

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.02.SimpleOperationsandCalculations(Exercises)-Exercises/03.2DRectangleArea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._2DRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+
+
+            var width = Math.Abs(x1 - x2);
+            var height = Math.Abs(y1 - y2);
+            var area = width * height;
+            var perimeter = 2 * (width + height);
+            Console.WriteLine($"{area:f2}");
+            Console.WriteLine($"{perimeter:f2}");
+        }
+    }
+}
