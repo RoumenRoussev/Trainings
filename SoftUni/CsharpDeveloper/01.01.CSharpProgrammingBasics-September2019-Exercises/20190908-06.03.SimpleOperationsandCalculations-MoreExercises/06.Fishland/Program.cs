8e718ecf894d1fe350e33ef7231bc08915e68fec
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-23 01:39:20 +0300

    06. Fishland

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.03.SimpleOperationsandCalculations-MoreExercises/06.Fishland/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06.Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double marcelPricePerKg = double.Parse(Console.ReadLine());
+            double spratPricePerKg = double.Parse(Console.ReadLine());
+            double beltedBonitoKg = double.Parse(Console.ReadLine());
+            double scadKg = double.Parse(Console.ReadLine());
+            double clamsKg = double.Parse(Console.ReadLine());
+
+            double beltedBonitoPricePerKg = marcelPricePerKg + (marcelPricePerKg * 0.6);
+            double beltedBonitoIncome = beltedBonitoPricePerKg * beltedBonitoKg;
+
+            double scadPricePerKg = spratPricePerKg + (spratPricePerKg * 0.8);
+            double scadIncome = scadPricePerKg * scadKg;
+
+            double totalIncome = scadIncome + beltedBonitoIncome + (clamsKg * 7.50);
+
+            Console.WriteLine($"{totalIncome:f2}");
+        }
+    }
+}
