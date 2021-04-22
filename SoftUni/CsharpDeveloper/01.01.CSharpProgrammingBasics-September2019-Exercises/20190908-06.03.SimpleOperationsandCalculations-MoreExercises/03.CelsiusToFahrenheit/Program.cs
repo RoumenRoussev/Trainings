b26b3a87e61bf10365494fa5f7fba67e559fdf62
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-23 01:38:48 +0300

    03. Celsius to Fahrenheit

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.03.SimpleOperationsandCalculations-MoreExercises/03.CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter the Temperature in Celsius(°C) : ");
+            // Console.WriteLine();
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine($"{fahrenheit:F2}");
+            //Console.WriteLine($"Temperature in Fahrenheit is(°F) : {faren:F2}");
+        }
+    }
+}
