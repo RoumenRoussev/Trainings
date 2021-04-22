bbb921bc45ddb4de86f8ad576f731ca68b5f07b4
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-23 01:39:31 +0300

    07. House Painting

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.03.SimpleOperationsandCalculations-MoreExercises/07.HousePainting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.HousePainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = double.Parse(Console.ReadLine());
+            var y = double.Parse(Console.ReadLine());
+            var h = double.Parse(Console.ReadLine());
+
+            if (x >= 2 && x <= 100 && y >= 2 && y <= 100 && h >= 2 && h <= 100)
+            {
+                var wallArea = ((x * x - 1.2 * 2) + x * x + 2 * (x * y - 1.5 * 1.5));
+                var roofArea = 2 * x * y + 2 * x * h / 2;
+
+                var green = wallArea / 3.4;
+                var red = roofArea / 4.3;
+
+                Console.WriteLine($"{green:f2}");
+                Console.WriteLine($"{red:f2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
