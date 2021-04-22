e795b0a890b454e7906dfda6c4d81f1e5ad4db02
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-23 01:40:16 +0300

    09. Weather Forecast - Part 2

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190908-06.03.SimpleOperationsandCalculations-MoreExercises/09.WeatherForecast-Part2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _09.WeatherForecast_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degree = double.Parse(Console.ReadLine());
+            if (degree >= 26.00 && degree <= 35.00)
+            {
+                Console.WriteLine("Hot");
+            }
+            if (degree >= 20.1 && degree <= 25.9)
+            {
+                Console.WriteLine("Warm");
+            }
+            if (degree >= 15.00 && degree <= 20.00)
+            {
+                Console.WriteLine("Mild");
+            }
+            if (degree >= 12.00 && degree <= 14.9)
+            {
+                Console.WriteLine("Cool");
+            }
+            if (degree >= 5.00 && degree <= 11.9)
+            {
+                Console.WriteLine("Cold");
+            }
+            if (degree < 5.00 || degree > 35.00)
+
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
