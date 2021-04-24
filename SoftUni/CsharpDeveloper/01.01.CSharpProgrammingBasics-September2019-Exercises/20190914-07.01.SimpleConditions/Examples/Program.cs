aa5f3ec1f5038e8e48c85aba16fb94d55210025b
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-24 07:30:28 +0300

    Examples

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190914-07.01.SimpleConditions/Examples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double withdrawAmount = double.Parse(Console.ReadLine());
+
+            bool canWithdraw = budget >= withdrawAmount;
+
+            if (canWithdraw)
+            {
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.WriteLine("Falls!");
+            }
+            Console.WriteLine("Good bye!");
+
+            //int a = 5;
+            //int b = 10;
+            //Console.WriteLine(b == 2 * a);
+
+
+
+            //var ocenka = double.Parse(Console.ReadLine());
+            //if (ocenka >= 5.50)
+            //{
+            //    Console.WriteLine("Excellent!");
+            //}
+        }
+    }
+}
