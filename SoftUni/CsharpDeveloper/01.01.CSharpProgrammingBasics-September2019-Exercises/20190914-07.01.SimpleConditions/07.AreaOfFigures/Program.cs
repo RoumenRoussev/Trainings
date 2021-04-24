f86309a4eda46a5eb38dbec60839ea8472bf3815
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-24 07:29:45 +0300

    07. Area of Figures

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190914-07.01.SimpleConditions/07.AreaOfFigures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _07.AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine().ToLower();
+            double area = 0;
+
+            if (figure.Equals("square"))
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+
+            }
+            else if (figure.Equals("rectangle"))
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                area = a * b;
+            }
+            else if (figure.Equals("circle"))
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI * radius * radius;
+            }
+            else if (figure.Equals("triangle"))
+            {
+                double side = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                area = (side * h) / 2;
+            }
+
+            Console.WriteLine($"{area:F3}");
+        }
+    }
+}
