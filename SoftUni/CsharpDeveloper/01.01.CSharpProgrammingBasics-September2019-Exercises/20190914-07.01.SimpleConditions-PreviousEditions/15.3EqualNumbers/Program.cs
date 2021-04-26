d18841186e88271005b62b1bfd4018a228b8c975
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-26 08:52:40 +0300

    15. 3 Equal Numbers

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190914-07.01.SimpleConditions-PreviousEditions/15.3EqualNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _15._3EqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+            double thirdNum = double.Parse(Console.ReadLine());
+
+            if (firstNum == secondNum && secondNum == thirdNum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
