3450d82d8727a7b7151f7d424f8b88c3f37ca016
roumenroussev <roumen.i.roussev@gmail.com>
2021-04-26 08:51:21 +0300

    07. Sum Seconds

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190914-07.01.SimpleConditions-PreviousEditions/07.SumSeconds/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07.SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstSeconds = int.Parse(Console.ReadLine());
+            int secondSeconds = int.Parse(Console.ReadLine());
+            int thirdSeconds = int.Parse(Console.ReadLine());
+
+            int totalSecond = firstSeconds + secondSeconds + thirdSeconds;
+
+            Console.Write("{0}:{1:00}", (totalSecond / 60), (totalSecond % 60));
+        }
+    }
+}
