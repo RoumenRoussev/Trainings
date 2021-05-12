ef7eb6d175d2a3de5be72e65b77fc8a7168fbf73
roumenroussev <roumen.i.roussev@gmail.com>
2021-05-13 00:52:33 +0300

    06. Godzilla vs. Kong

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190915-08.01.ConditionalStatements(Exercises)-Exercises/06.GodzillaVSKong/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.GodzillaVSKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budzhet = double.Parse(Console.ReadLine());
+            int brStatisti = int.Parse(Console.ReadLine());
+            double cenaOblekloEdinStatist = double.Parse(Console.ReadLine());
+            double dekor = 0.1 * budzhet;
+            double drehi = cenaOblekloEdinStatist * brStatisti;
+            //if (brStatisti > 150) drehi = drehi * 0.1;
+            if (brStatisti > 150) drehi = drehi * (1 - 0.1); // drehi = drehi * 0.1
+
+            if ((dekor + drehi) > budzhet)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine("Wingard needs {0:f2} leva more.", ((drehi + dekor) - budzhet));
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine("Wingard starts filming with {0:f2} leva left.", (budzhet - (drehi + dekor)));
+            }
+        }
+    }
+}
