07555183596a1ee16c466ec0f8f4d5da77efa777
roumenroussev <roumen.i.roussev@gmail.com>
2021-05-13 00:52:20 +0300

    05. Time + 15 Minutes

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190915-08.01.ConditionalStatements(Exercises)-Exercises/05.Time+15Minutes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05.Time_15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int totalTime = (hours * 60) + minutes + 15;
+            int hoursAfter = totalTime / 60;
+
+            if (hoursAfter == 24)
+            {
+                hoursAfter = 00;
+            }
+
+            Console.Write(hoursAfter);
+
+            if (totalTime % 60 < 9)
+            {
+                Console.Write(":0{0}", totalTime % 60);
+            }
+            else
+            {
+                Console.Write(":{0}", totalTime % 60);
+            }
+        }
+    }
+}
