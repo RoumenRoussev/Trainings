84ce2204598f07ea31d7bb381e5004ca22556932
roumenroussev <roumen.i.roussev@gmail.com>
2021-05-13 00:52:40 +0300

    07. World Swimming Record

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190915-08.01.ConditionalStatements(Exercises)-Exercises/07.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07.WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordSec = double.Parse(Console.ReadLine());
+            double distanceMeters = double.Parse(Console.ReadLine());
+            double timeSec = double.Parse(Console.ReadLine());
+            double distanceSec = distanceMeters * timeSec;
+            double bonusTime = Math.Floor(distanceMeters / 15) * 12.5;
+            double allTime = distanceSec + bonusTime;
+            if (recordSec > allTime)
+            {
+                double needTime = distanceSec + bonusTime;
+                Console.WriteLine($"Yes, he succeeded! The new world record is {needTime:F2} seconds.");
+            }
+            else
+            {
+                double left = Math.Abs(allTime - recordSec);
+                Console.WriteLine($"No, he failed! He was {left:F2} seconds slower.");
+            }
+        }
+    }
+}
