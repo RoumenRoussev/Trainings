3ddb925725f3a4ae53a116cd8b1ad88031e8ffe6
roumenroussev <roumen.i.roussev@gmail.com>
2021-06-08 06:53:30 +0300

    01. On Time for the Exam

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190928-09.02.ComplexConditions(Practice)-ExamProblems/01.OnTimeForThe xam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.OnTimeForThe_xam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
