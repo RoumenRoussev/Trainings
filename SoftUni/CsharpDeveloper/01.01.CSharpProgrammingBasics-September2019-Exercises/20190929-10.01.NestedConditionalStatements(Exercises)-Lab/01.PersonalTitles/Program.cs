a22db95ed55c0f3f3112742810643c2f3baf43a8
roumenroussev <roumen.i.roussev@gmail.com>
2021-06-09 07:06:26 +0300

    01. Personal Titles

---
+++ SoftUni/CsharpDeveloper/01.01.CSharpProgrammingBasics-September2019-Exercises/20190929-10.01.NestedConditionalStatements(Exercises)-Lab/01.PersonalTitles/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _01.PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+
+            if (gender == 'f')
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else if (gender == 'm')
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+
+
+
+            //if (gender == 'f' && age < 16)
+            //{
+            //    Console.WriteLine("Miss");
+            //}
+            //else if (gender == 'f' && age >= 16)
+            //{
+            //    Console.WriteLine("Ms.");
+            //}
+            //else if (gender == 'm' && age >= 16)
+            //{
+            //    Console.WriteLine("Master");
+            //}
+            //else if (gender == 'm' && age >= 16)
+            //{
+            //    Console.WriteLine("Mr.");
+            //}
+        }
+    }
+}
